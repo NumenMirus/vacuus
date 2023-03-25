3e0b1faed99cca1c8c23719da54cdb1f7ea19471
numen <michaelbastauni@gmail.com>
2023-03-25 13:28:24 +0100

    implementing xml parsing modularization

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@
 #include <fstream>
 #include <cstring>
 #include "lib/rapidxml.hpp"
+#include "lib/rapidxml_print.hpp"
 
 class Credentials{
+public:
     std::string site;
     std::string username;
     std::string password;
@@ -26,23 +28,35 @@ class Credentials{
         this->password = password;
     }
 
-    ~Credentials(){}
+    ~Credentials()= default;
 
     // securely decrypt password
-    void decrypt(){};
+    std::string* decrypt(){
+        return new std::string("ENC - " + this->password);
+    };
 
     // securely encrypt password
-    void encrypt(){};
+    std::string* encrypt(){
+        return new std::string("ENC - " + this->password);
+    };
 
     // return xml representation of this class to be added as node to db
-    void to_xml(){};
+    std::string* to_xml(){
+        auto* res = new std::string(
+                "<block><site name=\""
+                + this->site + "\"></site><username name=\""
+                + this->username + "\"></username><password name=\""
+                + this->password + "\"></password></block>"
+                );
+        return res;
+    };
 
     // prints a debug string
     void debug(){
         std::cout <<
             "Site: " << this->site <<
-            " Username: " << this->username <<
-            " Password: " << this->password << std::endl;
+            "\nUsername: " << this->username <<
+            "\nPassword: " << this->password << "\n\n";
     }
 };
 
@@ -65,35 +79,77 @@ char* file_to_char_array(std::string filename){
     return char_array;
 }
 
-rapidxml::xml_node<>* get_root_node(char *input){
+char* string_to_char_array(std::string *input){
 
-    rapidxml::xml_document<> doc;    // character type defaults to char
+    size_t length = input->size()+1; // get length of string, +1 for terminator /0
+    char *char_array = new char[length]; // instantiate char array with appropriate length
+    std::strncpy(char_array, input->c_str(), length); // copy string to char array to prevent const
+
+    return char_array;
+}
+
+rapidxml::xml_document<>* get_doc(std::string *input){
+    auto *doc = new rapidxml::xml_document<>;
+    char* data = string_to_char_array(input);
+    doc->parse<0>(data);
+
+    return doc;
+}
+
+rapidxml::xml_document<>* get_doc(char* input){
+    auto *doc = new rapidxml::xml_document<>;
+    doc->parse<0>(input);
+
+    return doc;
+}
+
+rapidxml::xml_node<>* get_root_node(rapidxml::xml_document<>* doc){
+
     rapidxml::xml_node<> *root_node;
-    doc.parse<0>(input);    // parsing file string - 0 means default parse flags
-    root_node = doc.first_node("root");     // get root node
+    root_node = doc->first_node("root");     // get root node
 
     return root_node;
 }
 
+rapidxml::xml_node<>* get_new_node_from_creds(rapidxml::xml_document<>* doc, Credentials *creds){
 
+    rapidxml::xml_node<>* new_node = doc->allocate_node(rapidxml::node_type(1));
+
+    new_node->append_attribute(doc->allocate_attribute("site", string_to_char_array(&creds->site)));
+    new_node->append_attribute(doc->allocate_attribute("username", string_to_char_array(&creds->username)));
+    new_node->append_attribute(doc->allocate_attribute("password", string_to_char_array(creds->encrypt())));
+
+    return new_node;
+}
+
+void print_tree(rapidxml::xml_node<>* root_node){
+    for(rapidxml::xml_node<> *block_node = root_node->first_node("block"); block_node; block_node = block_node->next_sibling()){
+        std::cout << "Block content: \n\t|_ " <<
+             block_node->first_attribute("site")->value() << "\n\t|_ " <<
+             block_node->first_attribute("username")->value() << "\n\t|_ " <<
+             block_node->first_attribute("password")->value() << "\n" <<
+             std::endl;
+    }
+
+}
 
 int main(){
     using namespace std;
     using namespace rapidxml;
 
+
     char *char_data = file_to_char_array("temp.xml"); // get file into a string to parse
+    xml_document<>* doc = get_doc(char_data); // get xml tree from string
+    xml_node<>* root_node = get_root_node(doc); //
 
-    xml_node<>* root_node = get_root_node(char_data); // parse string and get root
+    Credentials new_creds = Credentials("https://vs.co", "mario_rossi", "MyVscoPassword");
+    xml_node<>* new_node = get_new_node_from_creds(doc, &new_creds);
 
-    // print file values
-    for(xml_node<> *block_node = root_node->first_node("block"); block_node; block_node = block_node->next_sibling()){
-        cout << "Block content: \n\t|_ " <<
-            block_node->first_node("site")->first_attribute()->value() << "\n\t|_ " <<
-            block_node->first_node("username")->first_attribute()->value() << "\n\t|_ " <<
-            block_node->first_node("password")->first_attribute()->value() << "\n" <<
-            endl;
-    }
+    root_node->append_node(new_node);
 
+    print_tree(root_node);
+
+    cout << "Finished" << endl;
     delete[] char_data; // free char array before exiting program
     return 0;
 }
